77cac263f812c521158eda7c78ba4010f64944ec
PengMiMac <867321979@qq.com>
2021-04-12 10:57:47 +0800

    '64'

--- 64.py
+++ 64.py
@@ -1,17 +1,19 @@
 from typing import List
 
 # dp
-def minPathSum(grid: List[List[int]]) -> int:
-    m = len(grid)
-    n = len(grid[0])
-    for i in range(1, m):
-        grid[i][0] = grid[i-1][0] + grid[i][0]
-    for i in range(1, n):
-        grid[0][i] = grid[0][i-1] + grid[0][i]
-    for i in range(1, m):
-        for j in range(1, n):
-            grid[i][j] = min([grid[i-1][j], grid[i][j-1]]) + grid[i][j]
-    return grid[-1][-1]
+def minPathSum(matrix: List[List[int]]) -> int:
+    r = len(matrix)
+    c = len(matrix[0])
+    dp = [[0 for _ in range(c)] for _ in range(r)]
+    dp[0][0] = matrix[0][0]
+    for ci in range(1, c):
+        dp[0][ci] = dp[0][ci - 1] + matrix[0][ci]
+    for ri in range(1, r):
+        dp[ri][0] = dp[ri - 1][0] + matrix[ri][0]
+    for i in range(1, r):
+        for j in range(1, c):
+            dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + matrix[i][j]
+    return dp[-1][-1]
 
 
 grid = [[1,2,3],
